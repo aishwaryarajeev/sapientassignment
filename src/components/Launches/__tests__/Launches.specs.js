5cf4bbae6bc5167d4618691e740373484678f606
MishraA <aishwaryarajeev.mishra@hmhco.com>
2020-09-28 03:15:57 +0530

    screenshots for README.md

--- src/components/Launches/__tests__/Launches.specs.js
+++ src/components/Launches/__tests__/Launches.specs.js
@@ -6,13 +6,17 @@ import { Launches } from "../Launches";
 describe("<Launches />", () => {
   let wrapper = null;
   beforeEach(() => {
-    wrapper = shallow(<Launches />);
+    wrapper = shallow(
+      <Launches endPoint={"https://api.spaceXdata.com/v3/launches?limit=100"} />
+    );
   });
   it("should render correctly", () => {
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
-  it("should render correctly", () => {
-    wrapper = mount(<Launches />);
+  it("should mount correctly", () => {
+    wrapper = mount(
+      <Launches endPoint={"https://api.spaceXdata.com/v3/launches?limit=100"} />
+    );
     wrapper.update();
 
     expect(wrapper.find("Launch")).toBeTruthy();
